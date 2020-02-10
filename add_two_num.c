660e5532950b6dcd6fa1ac7ec77e259e6fe80c08
IAMMOIZ <56217551+IAMMOIZ@users.noreply.github.com>
2020-02-10 16:47:10 +0530

    Create add_two_num.c

---
+++ add_two_num.c
@@ -0,0 +1,18 @@
+//c program to add two number ;
+#include<stdio.h>
+int main()
+{
+    int first , second;
+    
+    //get first number from user
+    printf("Enter first number : ");
+    scanf("%d",&first);
+    
+    //get second number from user
+    printf("Enter second number : ");
+    scanf("%d",&second);
+    
+    //result
+    printf("addintion of %d and %d is %d",first,second,first+second);
+    
+}
