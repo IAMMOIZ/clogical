4f93040e49dd60af3df6d4afa9a63461d450c367
IAMMOIZ <56217551+IAMMOIZ@users.noreply.github.com>
2020-02-10 15:51:34 +0530

    Update c-logical-coding-question.c

--- c-logical-coding-question.c
+++ c-logical-coding-question.c
@@ -1,1 +1,7 @@
 1- swap two numbers by using thired variable-filename[swaptwonumberbyusingthired.c]
+2- swap two number without using thired variabel-filename[swaptwovarWithoutusingThired.c]
+3- reverse an array-filename[arrayReverse.c]
+4- fibonacci series upto nth digits[fibonacci.c]
+5- fibonacci series to nth terms[fibonacci_limit.c]
+6- fibonacci series upto nth digits[fibonacci_recursion.c]pending
+7- fibonacci series to nth terms[fibonacci_recursion_nthterms.c]pending
